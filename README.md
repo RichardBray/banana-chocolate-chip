019981b759f3c3a5eee915046a0dce602d4579e7
Richard Bray <richardobray@yahoo.co.uk>
2014-11-11 14:45:39 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Banana Chocolate Chip (Muffin)
+==============================
+
+A framework for modern web apps based on [Muffin](http://richbray.me/muffin/)
+
+##INSTALL
+
+1. Install the dependancies
+
+	npm install
+
+
+2. Run gulp
+
+	gulp
+
+
---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+
+// Don't think I use gulp-util for anything but oh well, you never know.
+
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	sass = require('gulp-ruby-sass'),
+	prefix = require('gulp-autoprefixer'),
+	minifyCSS = require('gulp-minify-css'),
+	concat = require('gulp-concat'),
+	webserver = require('gulp-webserver'),
+	uglify = require('gulp-uglify');
+	rename = require('gulp-rename');
+	minifyHTML = require('gulp-minify-html');
+	imagemin = require('gulp-imagemin');
+	pngcrush = require('imagemin-pngcrush');
+
+// 1. Webserver
+gulp.task('webserver', function() {
+	gulp.src('dist/')
+	.pipe(webserver({
+		livereload: true,
+		port: '8080'
+	}));
+});
+
+// 2. SASS Minification and conversion to CSS
+gulp.task('styles', function () {
+    gulp.src('app/assets/styles/*.scss')
+        .pipe(sass())
+		.on('error', function (err) { console.log(err.message); })
+        .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+        .pipe(minifyCSS())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/assets/styles'))
+});
+
+// 3. JS Minificaiton and concatination
+gulp.task('scripts', function () {
+	gulp.src('app/assets/scripts/*.js')
+		.pipe(concat("script.min.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/assets/scripts'))
+});
+
+// 4. HTML Minification
+gulp.task('html', function() {
+	var opts = {comments:true,spare:true};
+	gulp.src('app/**/*.html')
+	.pipe(minifyHTML(opts))
+	.pipe(gulp.dest('dist/'))
+});
+
+// 5. Minify PNG, JPEG, GIF and SVG images
+gulp.task('images', function () {
+    return gulp.src('app/assets/img/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest('dist/assets/img'));
+});
+
+// 6. Copying just the fonts from 'app' to 'dest'
+gulp.task('fonts', function () {
+	return gulp.src(['app/assets/fonts/*'])
+	.pipe(gulp.dest('dist/assets/fonts'))
+});
+
+// 7. Copying all the other files from 'app' to 'dest'
+gulp.task('copy', function () {
+  return gulp.src([
+    'app/*',
+    '!app/*.html', //needed for html minification
+    '!app/Gemfile'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'))
+});
+
+// Watching files for changes
+gulp.task('watch', function () {
+	gulp.watch('app/assets/styles/*.scss', ['styles']);
+	gulp.watch('app/assets/scripts/*.js', ['scripts']);
+	gulp.watch('app/assets/img/**/*', ['images']);
+	gulp.watch('app/**/*.html', ['html']);
+});
+
+
+gulp.task('default', ['webserver','copy', 'html', 'styles', 'fonts', 'scripts', 'images', 'watch']);
+
+
